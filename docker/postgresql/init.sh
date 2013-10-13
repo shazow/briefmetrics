1727698f9dd5fcdb570da18ef46878b63263985d
Andrey Petrov <andrey.petrov@shazow.net>
2013-10-13 12:16:31 +0200

    Giving up on psql sock sharing.

--- docker/postgresql/init.sh
+++ docker/postgresql/init.sh
@@ -2,25 +2,27 @@
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
+IP_ADDRESS="$(ip addr show eth0 | grep 'inet ' | awk '{ print $2 }' | cut -d '/' -f1)"
+
+POSTGRES_BIN="/usr/lib/postgresql/9.3/bin/postgres"
+POSTGRES_CONFIG="/etc/postgresql/9.3/main/postgresql.conf"
+
+
 if [ ! -d /var/lib/postgresql/9.3/main ] ; then
-	pg_dropcluster 9.3 main --stop 2> /dev/null
-	pg_createcluster 9.3 main
-	cat > "/etc/postgresql/9.3/main/pg_hba.conf" << EOF
+    cat > "/etc/postgresql/9.3/main/pg_hba.conf" << EOF
 local   all             postgres                                trust
 host    all             all         0.0.0.0/0                   md5
 EOF
-	pg_ctlcluster 9.3 main start
-	password=${MASTER_PASSWORD:-$(< /dev/urandom tr -dc A-Za-z0-9 | head -c 32)}
-	psql -U postgres -c "ALTER USER postgres with password '$password';" >/dev/null 2>&1
-	echo "  pass   $password"
-	echo "  user   postgres"
-	pg_ctlcluster 9.3 main stop
-	exit 0
+    pg_ctlcluster 9.3 main start
+    password=${MASTER_PASSWORD:-$(< /dev/urandom tr -dc A-Za-z0-9 | head -c 16)}
+    psql -U postgres -c "ALTER USER postgres WITH PASSWORD '$password';" &> /dev/null
+    psql -U postgres -c "CREATE DATABASE db;"
+
+    pg_ctlcluster 9.3 main stop
+
+    echo "  url=\"postgres://postgres:$password@$IP_ADDRESS/db\""
+
+    exit 0
 fi
 
-if [ "$CREATE_DB" ]; then
-    su postgres -c "createdb -O postgres \"$CREATE_DB\""
-fi
-
-pgtune -i /etc/postgresql/9.3/main/postgresql.conf -o /etc/postgresql/9.3/main/postgresql.conf  -TWeb -M`free  -ob| grep Mem: | awk '{ printf "%d",$2 }'`
-su postgres -c "/usr/lib/postgresql/9.3/bin/postgres -c config_file='/etc/postgresql/9.3/main/postgresql.conf' -c unix_socket_directories='/app/socks'"
+su postgres -c "$POSTGRES_BIN -c config_file=\"$POSTGRES_CONFIG\" -c listen_address='*'"
