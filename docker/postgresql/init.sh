8c1f9fbda0299801b01c312bcae5e5598aa67b0c
Andrey Petrov <andrey.petrov@shazow.net>
2013-10-14 15:19:37 +0200

    Credentials tweak, and subnet pg_hba.

--- docker/postgresql/init.sh
+++ docker/postgresql/init.sh
@@ -3,6 +3,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
 IP_ADDRESS="$(ip addr show eth0 | grep 'inet ' | awk '{ print $2 }' | cut -d '/' -f1)"
+IP_SUBNET="$(echo $IP_ADDRESS | cut -d '.' -f1-3).0/16"
 
 POSTGRES_BIN="/usr/lib/postgresql/9.3/bin/postgres"
 POSTGRES_CONFIG="/etc/postgresql/9.3/main/postgresql.conf"
@@ -12,7 +13,7 @@ if [ ! -d "/var/lib/postgresql/9.3/main" ] ; then
     pg_createcluster 9.3 main
     cat > "/etc/postgresql/9.3/main/pg_hba.conf" << EOF
 local   all             postgres                                trust
-host    all             all         0.0.0.0/0                   md5
+host    all             all         $IP_SUBNET                  md5
 EOF
     pg_ctlcluster 9.3 main start
     password=${MASTER_PASSWORD:-$(< /dev/urandom tr -dc A-Za-z0-9 | head -c 16)}
@@ -21,7 +22,7 @@ EOF
 
     pg_ctlcluster 9.3 main stop
 
-    echo "* Credentials: url=\"postgres://postgres:$password@$IP_ADDRESS/db\""
+    echo "* Credentials: url=\"postgresql://postgres:$password@$IP_ADDRESS/db\""
     exit 0
 fi
 
