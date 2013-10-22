3bbaaf9497fa1bd5318785f3a07ade1fed8c9f58
Andrey Petrov <andrey.petrov@shazow.net>
2013-10-22 23:37:23 +0200

    Base migration.

---
+++ migration/versions/0000_base.py
@@ -0,0 +1,16 @@
+"""Base migration.
+
+Revision ID: 0000
+Revises: None
+Create Date: 2013-01-01 00:00:00.000000
+
+"""
+
+revision = '0000'
+down_revision = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
