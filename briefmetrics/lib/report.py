ff74d1ac1388695a8f0a888bfb8a80a61ec8fa6c
Andrey Petrov <andrey.petrov@shazow.net>
2013-11-28 18:17:39 -0500

    fix casting more?

--- briefmetrics/lib/report.py
+++ briefmetrics/lib/report.py
@@ -11,6 +11,16 @@ def _prune_abstract(label):
         return
     return label
 
+def _cast_bounce(v):
+    if not v:
+        return
+    return float(v) / 100.0
+
+def _cast_time(v):
+    if not v:
+        return None
+    return float(v)
+
 
 class Column(object):
     def __init__(self, id, label=None, type_cast=None, type_format=None, visible=None, reverse=False, average=None, threshold=None):
@@ -294,16 +304,11 @@ class WeeklyReport(Report):
 
     def fetch(self, google_query):
 
-        def _cast_bounce(v):
-            if not v:
-                return
-            return float(v) / 100.0
-
         # Summary
         summary_metrics = [
             Column('ga:pageviews', label='Views', type_cast=int, type_format=h.human_int, threshold=0, visible=0),
             Column('ga:uniquePageviews', label='Uniques', type_cast=int, type_format=h.human_int),
-            Column('ga:avgTimeOnSite', label='Time On Site', type_cast=lambda v: float(v) or None, type_format=h.human_time, threshold=0),
+            Column('ga:avgTimeOnSite', label='Time On Site', type_cast=_cast_time, type_format=h.human_time, threshold=0),
             Column('ga:visitBounceRate', label='Bounce Rate', type_cast=_cast_bounce, type_format=h.human_percent, reverse=True, threshold=0),
         ]
         self.tables['summary'] = google_query.get_table(
