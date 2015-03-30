9bdfba6a21bc8eaab1bf2f93d12ae3ba582c9af9
Andrey Petrov <andrey.petrov@shazow.net>
2015-03-29 18:05:51 -0700

    ini file fix.

--- briefmetrics/web/wsgi.py
+++ briefmetrics/web/wsgi.py
@@ -1,7 +1,9 @@
 import os
 here = os.path.dirname(__file__)
 
-ini_path = os.path.join(here, '../../production.ini')
+ini_path = os.environ.get('INI_FILE')
+if not ini_path:
+    ini_path = os.path.join(here, '../../production.ini')
 
 from paste.deploy import loadapp
 application = loadapp('config:%s' % ini_path, relative_to='.')
