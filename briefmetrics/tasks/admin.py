200d6e4062ef7412376f79e2609fcfd15af88586
Andrey Petrov <andrey.petrov@shazow.net>
2016-04-13 18:11:11 -0700

    Add started time to error test

--- briefmetrics/tasks/admin.py
+++ briefmetrics/tasks/admin.py
@@ -1,6 +1,9 @@
+import time
 from .setup import celery
 
+_started = time.time()
 
 @celery.task(ignore_result=True)
 def test_errors(message):
-    raise Exception(message)
+    msg = "_started = %f\n\nOriginal message: %s" % (_started, message)
+    raise Exception(msg)
--- briefmetrics/web/views/admin.py
+++ briefmetrics/web/views/admin.py
@@ -163,7 +163,7 @@ class AdminController(Controller):
     def test_errors(self):
         api.account.get_admin(self.request)
 
-        tasks.admin.test_errors.delay("This is a test.")
+        tasks.admin.test_errors.delay("Testing errors. _started: %f" % tasks._started)
 
         raise Exception("This is a test.")
 
