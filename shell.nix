2d770ec3ae37bf61ca39dc075abcb7c528c33a3b
Andrey Petrov <andrey.petrov@shazow.net>
2019-11-06 13:36:02 -0500

    shell.nix update

--- shell.nix
+++ shell.nix
@@ -19,9 +19,11 @@ stdenv.mkDerivation {
     libxml2
   ];
 
-  LIBRARY_PATH="${zlib.out}/lib:${zlib.dev}/include:${libxml2.out}/lib";
+  # FIXME: Not sure if libxslt is necessary
+  LIBRARY_PATH="${zlib.out}/lib:${zlib.dev}/include:${pkgs.libjpeg.out}/lib:${pkgs.libxml2.out}/lib:${pkgs.libxslt.out}/lib";
   LDFLAGS="-L${zlib.out}/lib -L${libjpeg.out}/lib";
   CFLAGS="-I${zlib.dev}/include -I${libjpeg.dev}/include";
+  C_INCLUDE_PATH="${pkgs.libjpeg.dev}/include:${pkgs.libjpeg}/include:${pkgs.libxml2.dev}/include/libxml2:${pkgs.libxslt.dev}/include:${pkgs.zlib.dev}/include";
 
   shellHook = ''
     export HISTFILE="$PWD/.bash_history";
