1c4269268cc3d1a59afa48b841c79ec37e3c4b7c
Andrey Petrov <andrey.petrov@shazow.net>
2019-11-07 16:22:45 -0500

    docker: Make psql work

--- docker/uwsgi/init.sh
+++ docker/uwsgi/init.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-python -m venv --system-site-packages /home/app/env
-. /home/app/env/bin/activate
+set -x -o pipefail
+
+if [[ ! -f "/home/app/env/bin/activate" ]]; then
+    python -m venv --system-site-packages /home/app/env
+    . /home/app/env/bin/activate
+    pip install psycopg2 PasteDeploy
+else
+    . /home/app/env/bin/activate
+fi
 
 cd /home/app/src
 python setup.py develop
