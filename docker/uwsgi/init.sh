6234465066e9f3701dda4131b46f6898e6b3e178
Andrey Petrov <andrey.petrov@shazow.net>
2019-12-31 12:00:03 -0500

    docker: Update uwsgi

--- docker/uwsgi/init.sh
+++ docker/uwsgi/init.sh
@@ -4,12 +4,12 @@ set -x -o pipefail
 if [[ ! -f "/home/app/env/bin/activate" ]]; then
     python -m venv --system-site-packages /home/app/env
     . /home/app/env/bin/activate
-    pip install psycopg2 PasteDeploy
 else
     . /home/app/env/bin/activate
 fi
 
 cd /home/app/src
+pip install psycopg2-binary PasteDeploy
 python setup.py develop
 make -e "INI_FILE=${INI_FILE}"
 uwsgi --ini "${INI_FILE}"
