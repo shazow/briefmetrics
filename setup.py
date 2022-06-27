5f8597ce47725798a8414900ab1a298e95c751a5
Andrey Petrov <andrey.petrov@shazow.net>
2022-06-27 11:01:02 -0400

    setup.py: Backport old changes

--- setup.py
+++ setup.py
@@ -5,25 +5,27 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-setup(name='briefmetrics',
-      version='2.1.0',
-      description='Briefmetrics emails you simple overviews of your website\'s Google Analytics.',
-      classifiers=[
+setup(
+    name="briefmetrics",
+    version="2.1.0",
+    description="Briefmetrics emails you simple overviews of your website's Google Analytics.",
+    classifiers=[
         "Programming Language :: Python",
-        "Framework :: Pylons",
+        "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+    ],
+    author="Andrey Petrov",
+    author_email="andrey.petrov@shazow.net",
+    url="https://briefmetrics.com/",
+    keywords="web wsgi pylons pyramid",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    test_suite="briefmetrics",
+    entry_points={
+        "paste.app_factory": [
+            "main = briefmetrics.web.environment:setup_wsgi",
         ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web wsgi pylons pyramid',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      test_suite='briefmetrics',
-      entry_points = """\
-      [paste.app_factory]
-      main = briefmetrics.web.environment:setup_wsgi
-      """,
-      )
+    },
+)
